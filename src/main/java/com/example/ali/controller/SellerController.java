a8eda58f38688b49833dbaa77b687117f161216c
Inhee <whdlsgml1599@gmail.com>
2023-09-20 01:21:24 +0900

    Store 전체 정보조회, 수정, 삭제 기능 구현

--- src/main/java/com/example/ali/controller/SellerController.java
+++ src/main/java/com/example/ali/controller/SellerController.java
@@ -1,24 +1,46 @@
 package com.example.ali.controller;
 
 import com.example.ali.dto.SellerSignupRequestDto;
+import com.example.ali.dto.StoreRequestDto;
+import com.example.ali.security.SellerDetailsImpl;
 import com.example.ali.service.SellerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/auth/seller")
 public class SellerController {
 
     private final SellerService sellerService;
 
-    @PostMapping("/signup")
+    @PostMapping("/auth/seller/signup")
     public ResponseEntity<?> signup(@RequestBody SellerSignupRequestDto requestDto){
         return sellerService.signup(requestDto);
     }
+
+    @GetMapping("/api/seller/store")
+    public ResponseEntity<?> getStores(){
+        return sellerService.getStores();
+    }
+
+    @PutMapping("/api/seller/store")
+    public ResponseEntity<?> updateStore(@RequestBody StoreRequestDto requestDto){
+        return sellerService.updateStore(requestDto);
+    }
+
+    @DeleteMapping("/api/seller/store")
+    public ResponseEntity<?> deleteStore(@AuthenticationPrincipal SellerDetailsImpl sellerDetails){
+        return sellerService.deleteStore(sellerDetails.getSeller());
+    }
+
+
 }
