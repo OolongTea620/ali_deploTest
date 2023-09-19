d8bb6255d3866b22f9ae7d67185d871ac6a9b1b4
heypop <hey@herenas.com>
2023-09-19 23:45:26 +0900

    USER, SELLER 회원가입, 로그인, 토큰발행 기능 확인

--- src/main/java/com/example/ali/jwt/JwtUtil.java
+++ src/main/java/com/example/ali/jwt/JwtUtil.java
@@ -38,7 +38,7 @@ public class JwtUtil {
     // Token 식별자
     public static final String BEARER_PREFIX = "Bearer ";
     // 토큰 만료시간
-    private final long ACCESS_TIME =  60 * 1000L; // 1분, 밀리세컨드
+    private final long ACCESS_TIME =  10 * 60 * 1000L ; // 1분, 밀리세컨드
     private final long REFRESH_TIME  = 60 * 60 * 1000L * 24; // 1일
 
     public static final String ACCESS_TOKEN = "Access_Token";
