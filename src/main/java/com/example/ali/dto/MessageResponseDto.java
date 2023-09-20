8da3ba900293cb851ee1f4b957d767da6a8d444a
Inhee <whdlsgml1599@gmail.com>
2023-09-20 09:49:03 +0900

    createProduct compile error 해결

---
+++ src/main/java/com/example/ali/dto/MessageResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.ali.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MessageResponseDto {
+    private String msg;
+}
