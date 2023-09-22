a90a391137dd70742d992d5ac5cf85b9674f1238
DESKTOP-HMN2L1J\정강용 <tuto3355@naver.com>
2023-09-22 09:37:42 +0900

    서비스단에서 dto, 컨트롤러에서 responseEntitiy 반환하도록 수정

--- src/main/java/com/example/ali/controller/ProductController.java
+++ src/main/java/com/example/ali/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.example.ali.controller;
 
+import com.example.ali.dto.MessageDataResponseDto;
 import com.example.ali.dto.ProductRequestDto;
+import com.example.ali.dto.ProductResponseDto;
 import com.example.ali.security.SellerDetailsImpl;
 import com.example.ali.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,37 +27,48 @@ public class ProductController {
     //seller 상품 등록
     @Operation(summary = "seller 상품 등록")
     @PostMapping("/seller/product")
-    public ResponseEntity<?> createProduct(
+    public ResponseEntity<MessageDataResponseDto> createProduct(
             @AuthenticationPrincipal SellerDetailsImpl sellerDetails,
             @RequestPart(value = "image", required = false) MultipartFile image,
             @RequestPart(value = "requestDto", required = false) ProductRequestDto requestDto) throws IOException {
-        return productService.createProduct(requestDto, sellerDetails.getSeller(), image);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(productService.createProduct(requestDto, sellerDetails.getSeller(), image));
     }
 
     //seller 상품 수정
     @Operation(summary = "seller 상품 수정")
     @PutMapping("/seller/product/{productId}")
-    public ResponseEntity<?> updateProduct(@PathVariable Long productId,
+    public ResponseEntity<MessageDataResponseDto> updateProduct(@PathVariable Long productId,
                                            @RequestBody ProductRequestDto requestDto) {
-        return productService.updateProduct(productId, requestDto);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(productService.updateProduct(productId, requestDto));
     }
 
     //seller 상품 삭제
     @Operation(summary = "seller 상품 삭제")
     @DeleteMapping("/seller/product/{productId}")
-    public ResponseEntity<?> deleteProduct(@PathVariable Long productId) {
-        return productService.deleteProduct(productId);
+    public ResponseEntity<MessageDataResponseDto> deleteProduct(@PathVariable Long productId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.deleteProduct(productId));
     }
 
     //전체, 검색 상품 조회
     @Operation(summary = "선택 상품 조회")
     @GetMapping("/seller/products")
-    public ResponseEntity<?> getSearchProduct(@RequestParam String keyword) {
-        return productService.getSearchProduct(keyword);
+    public ResponseEntity<List<ProductResponseDto>> getSearchProduct(@RequestParam String keyword) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.getSearchProduct(keyword));
     }
+
     @Operation(summary = "전체 상품 조회")
     @GetMapping("/products")
-    public ResponseEntity<?> getProducts() {
-        return productService.getProducts();
+    public ResponseEntity<List<ProductResponseDto>> getProducts() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.getProducts());
     }
 }
