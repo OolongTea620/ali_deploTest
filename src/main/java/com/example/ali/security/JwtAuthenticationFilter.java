d61e8049ba7a863e46ef2cad05a0a3c717dcc0a7
Inhee <whdlsgml1599@gmail.com>
2023-09-22 11:18:31 +0900

    CustomLoginService 리펙토링 및 token 발급 로직 변경

--- src/main/java/com/example/ali/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/ali/security/JwtAuthenticationFilter.java
@@ -81,8 +81,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                         )
                 );
             } else {
-                UserDetails userDetails = userDetailsService.loadUserByUsername(requestDto.getUsername());
-//                log.info("userDetails.getUsername()={}",userDetails.getUsername());
+                UserDetails userDetails = customLoginService.loadUserByUsername(requestDto.getUsername());
                 return getAuthenticationManager().authenticate( // AuthenticationManager로 인증 시도 후 성공하면 Authentication 객체 리턴
                         new UsernamePasswordAuthenticationToken( // UsernamePasswordAuthenticationToken 객체를 Authentication 객체로 인증 시도
                                 requestDto.getUsername(), // principal
