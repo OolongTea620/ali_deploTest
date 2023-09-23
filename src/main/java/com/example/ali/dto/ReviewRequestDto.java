749554c523b2c1911a88b98ad5bf2bf452d907b6
DESKTOP-HMN2L1J\정강용 <tuto3355@naver.com>
2023-09-23 17:05:03 +0900

    review 관련 코드 리팩토링, 중복리뷰작성방지, 뷰에서 product status 관련오류(대소문자) 수정

--- src/main/java/com/example/ali/dto/ReviewRequestDto.java
+++ src/main/java/com/example/ali/dto/ReviewRequestDto.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ReviewRequestDto {
     private Long orderId;
 
--- src/main/java/com/example/ali/service/ReviewService.java
+++ src/main/java/com/example/ali/service/ReviewService.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -28,71 +29,62 @@ public class ReviewService {
     private final OrdersRepository ordersRepository;
     private final ProductRepository productRepository;
 
-    public ResponseEntity<List<ReviewResponseDto>> getProductReviewList(Long productId) {
+    public List<ReviewResponseDto> getProductReviewList(Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new IllegalArgumentException("해당하는 상품이 존재하지 않습니다"));
         
-        // Order_id로 조인하고 그 결과fmf가져오기
+        // Order_id로 조인하고 그 결과를가져오기
         List<Orders> orders = ordersRepository.findAllByProduct(product);
         List<Review> productReviews = reviewRepository.findAllByOrdersIn(orders);
 
-        return ResponseEntity.ok(
-                productReviews.stream()
+        return productReviews.stream()
                         .map(ReviewResponseDto::new)
-                        .toList()
-        );
+                        .toList();
     }
 
     @Transactional
-    public ResponseEntity<ReviewResponseDto> createReview(ReviewRequestDto requestDto, User user) {
+    public ReviewResponseDto createReview(ReviewRequestDto requestDto, User user) {
         Orders order = ordersRepository.findById(requestDto.getOrderId())
                 .orElseThrow(() -> new NullPointerException("해당하는 주문이 존재하지 않습니다"));
 
         if (!(order.getUser().getId().equals(user.getId()))) {
             throw new IllegalArgumentException("작성 권한이 없는 유저 입니다.");
         }
-//        if (!order.getUser().equals(user)) {
-//            throw new IllegalArgumentException("작성 권한이 없는 유저 입니다.");
-//        }
+
+        Optional<Review> oldreview = reviewRepository.findByOrders_Id(requestDto.getOrderId());
+        if (oldreview.isPresent()) {
+            throw new IllegalArgumentException("이미 작성한 리뷰가 있습니다");
+        }
+
         Review review = reviewRepository.save(new Review(requestDto, order));
 
-        return ResponseEntity
-                .status(HttpStatus.CREATED.value())
-                .body(new ReviewResponseDto(review));
+        return new ReviewResponseDto(review);
     }
 
     @Transactional
-    public ResponseEntity<ReviewResponseDto> updateReview(ReviewRequestDto requestDto, User user) {
+    public ReviewResponseDto updateReview(ReviewRequestDto requestDto, User user) {
         Review review = reviewRepository.findByOrders_Id(requestDto.getOrderId())
                 .orElseThrow(() -> new NullPointerException("해당하는 리뷰가 존재하지 않습니다"));
-        log.info("reiview.getId()={}", review.getId());
+
         if (!(review.getOrders().getUser().getId().equals(user.getId()))) {
             throw new IllegalArgumentException("작성 권한이 없는 유저 입니다.");
         }
 
-
-//        if (!(review.getOrders().getUser().equals(user))) {
-//            throw new IllegalArgumentException("작성 권한이 없는 유저 입니다.");
-//        }
-//
         review.update(requestDto);
-        return ResponseEntity.accepted().body(new ReviewResponseDto(review));
+        return new ReviewResponseDto(review);
     }
 
     @Transactional
-    public ResponseEntity<MessageResponseDto> deleteReview(Long orderId, User user) {
+    public MessageResponseDto deleteReview(Long orderId, User user) {
         Review review = reviewRepository.findByOrders_Id(orderId)
                 .orElseThrow(() -> new NullPointerException("해당하는 리뷰가 존재하지 않습니다"));
 
         if (!(review.getOrders().getUser().getId().equals(user.getId()))) {
             throw new IllegalArgumentException("삭제 권한이 없는 유저 입니다.");
         }
-//
-//        if (!(review.getOrders().getUser().equals(user))) {
-//            throw new IllegalArgumentException("삭제 권한이 없는 유저 입니다.");
-//        }
+
         reviewRepository.delete(review);
-        return ResponseEntity.ok(new MessageResponseDto("삭제 성공"));
+        return new MessageResponseDto("삭제 성공");
     }
 
 }
