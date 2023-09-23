80d40acd670722ca2a29f1d170cf1cf1d289ed63
Inhee <whdlsgml1599@gmail.com>
2023-09-23 16:46:36 +0900

    UserServiceTest verify error solve

--- src/test/java/com/example/ali/service/UserServiceTest.java
+++ src/test/java/com/example/ali/service/UserServiceTest.java
@@ -2,6 +2,8 @@ package com.example.ali.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -22,8 +24,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -105,7 +105,7 @@ class UserServiceTest {
         UserWalletResponseDto userWalletResponseDto = userService.getUserPoint(user);
 
         //then
-        verify(userRepository, times(1)).findById(user.getId());
+        // userService is not a mock -> verify(x)
         assertThat(userWalletResponseDto.getPoint()).isEqualTo(1000);
 
     }
