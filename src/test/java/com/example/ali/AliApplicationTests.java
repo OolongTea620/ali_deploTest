691c41984605c85cea54e6955dafd5c0336ecd51
tuto3355 <130144714+tuto3355@users.noreply.github.com>
2023-09-24 13:48:37 +0900

    Update AliApplicationTests.java

--- src/test/java/com/example/ali/AliApplicationTests.java
+++ src/test/java/com/example/ali/AliApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.ali;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class AliApplicationTests {
 
     @Test
